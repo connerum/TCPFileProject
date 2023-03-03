642d8ce0501cf0ac47788d68e14bcbf666e358d3
connerum <98507011+connerum@users.noreply.github.com>
2023-03-03 14:55:53 -0500

    Added functional code to FileClient & FileServer

--- src/main/java/edu/bsu/FileClient.java
+++ src/main/java/edu/bsu/FileClient.java
@@ -1,4 +1,109 @@
 package edu.bsu;
 
+import java.io.*;
+import java.net.*;
+
 public class FileClient {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java FileClient <server IP address> <server port number>");
+            System.exit(1);
+        }
+
+        String serverIP = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (
+                Socket socket = new Socket(serverIP, portNumber);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                BufferedReader console = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            System.out.println("Connected to file server at " + serverIP + ":" + portNumber);
+
+            while (true) {
+                System.out.print("Enter a command (L)ist, (D)elete, (R)ename, (U)pload, (O)ownload, or (E)xit: ");
+                String command = console.readLine();
+                if (command.equals("E")) {
+                    output.writeByte('E'); // Exit command
+                    break;
+                }
+                output.writeByte(command.charAt(0));
+
+                if (command.equals("L")) {
+                    String fileName;
+                    while (!(fileName = input.readUTF()).equals("END")) {
+                        System.out.println(fileName);
+                    }
+                } else if (command.equals("D")) {
+                    String fileName = console.readLine();
+                    output.writeUTF(fileName);
+
+                    if (input.readByte() == 'S') {
+                        System.out.println("File deleted successfully.");
+                    } else {
+                        System.out.println("File deletion failed.");
+                    }
+                } else if (command.equals("R")) {
+                    String oldFileName = console.readLine();
+                    String newFileName = console.readLine();
+                    output.writeUTF(oldFileName);
+                    output.writeUTF(newFileName);
+
+                    if (input.readByte() == 'S') {
+                        System.out.println("File renamed successfully.");
+                    } else {
+                        System.out.println("File renaming failed.");
+                    }
+                } else if (command.equals("U")) {
+                    String fileName = console.readLine();
+                    output.writeUTF(fileName);
+                    File file = new File(fileName);
+
+                    if (!file.exists()) {
+                        System.out.println("File not found.");
+                    } else {
+                        try (FileInputStream fis = new FileInputStream(file)) {
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+                            while ((bytesRead = fis.read(buffer)) != -1) {
+                                output.write(buffer, 0, bytesRead);
+                            }
+                            if (input.readByte() == 'S') {
+                                System.out.println("File uploaded successfully.");
+                            } else {
+                                System.out.println("File upload failed.");
+                            }
+                        } catch (IOException e) {
+                            System.err.println("Error: " + e.getMessage());
+                        }
+                    }
+                } else if (command.equals("O")) {
+                    String fileName = console.readLine();
+                    output.writeUTF(fileName);
+
+                    if (input.readByte() == 'F') {
+                        System.out.println("File not found.");
+                    } else {
+                        try (FileOutputStream fos = new FileOutputStream(fileName)) {
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+                            while ((bytesRead = input.read(buffer)) != -1) {
+                                fos.write(buffer, 0, bytesRead);
+                            }
+                            System.out.println("File downloaded successfully.");
+                        } catch (IOException e) {
+                            System.err.println("Error: " + e.getMessage());
+                        }
+                    }
+                } else {
+                    System.out.println("Invalid command.");
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Error: Unknown host " + serverIP);
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/edu/bsu/FileServer.java
+++ src/main/java/edu/bsu/FileServer.java
@@ -1,4 +1,129 @@
 package edu.bsu;
 
+import java.io.*;
+import java.net.*;
+
 public class FileServer {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java FileServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            System.out.println("File server is listening on port " + portNumber);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket.getInetAddress().getHostAddress());
+                new ServerThread(clientSocket).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
+
+class ServerThread extends Thread {
+    private Socket clientSocket;
+
+    public ServerThread(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    public void run() {
+        try (
+                InputStream input = clientSocket.getInputStream();
+                OutputStream output = clientSocket.getOutputStream();
+                DataInputStream dataInput = new DataInputStream(input);
+                DataOutputStream dataOutput = new DataOutputStream(output);
+        ) {
+            byte command = dataInput.readByte();
+
+            if (command == 'L') {
+                // List files
+                File folder = new File(".");
+                File[] files = folder.listFiles();
+
+                for (File file : files) {
+                    if (file.isFile()) {
+                        dataOutput.writeUTF(file.getName());
+                    }
+                }
+                dataOutput.writeUTF("END");
+            } else if (command == 'D') {
+                // Delete file
+                String fileName = dataInput.readUTF();
+                File file = new File(fileName);
+
+                if (file.delete()) {
+                    dataOutput.writeByte('S'); // Success
+                } else {
+                    dataOutput.writeByte('F'); // Failure
+                }
+            } else if (command == 'R') {
+                // Rename file
+                String oldFileName = dataInput.readUTF();
+                String newFileName = dataInput.readUTF();
+                File oldFile = new File(oldFileName);
+                File newFile = new File(newFileName);
+
+                if (oldFile.renameTo(newFile)) {
+                    dataOutput.writeByte('S'); // Success
+                } else {
+                    dataOutput.writeByte('F'); // Failure
+                }
+            } else if (command == 'U') {
+                // Upload file
+                String fileName = dataInput.readUTF();
+                File file = new File(fileName);
+
+                if (file.exists()) {
+                    dataOutput.writeByte('F'); // Failure
+                } else {
+                    try (FileOutputStream fos = new FileOutputStream(file)) {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = dataInput.read(buffer)) != -1) {
+                            fos.write(buffer, 0, bytesRead);
+                        }
+                        dataOutput.writeByte('S'); // Success
+                    } catch (IOException e) {
+                        dataOutput.writeByte('F'); // Failure
+                    }
+                }
+            } else if (command == 'O') {
+                // Download file
+                String fileName = dataInput.readUTF();
+                File file = new File(fileName);
+
+                if (!file.exists()) {
+                    dataOutput.writeByte('F'); // Failure
+                } else {
+                    try (FileInputStream fis = new FileInputStream(file)) {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = fis.read(buffer)) != -1) {
+                            dataOutput.write(buffer, 0, bytesRead);
+                        }
+                        dataOutput.writeByte('S'); // Success
+                    } catch (IOException e) {
+                        dataOutput.writeByte('F'); // Failure
+                    }
+                }
+            }
+            clientSocket.shutdownOutput();
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+    }
 }
